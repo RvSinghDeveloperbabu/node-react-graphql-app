e933d8e7280834e0b102bee4cf918b737cd16ff2
ravi kumar <ravikumar.developerbabu@gmail.com>
2020-07-11 17:18:24 +0530

    grapql query

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var graphqlHTTP = require('express-graphql');
+var {graphqlHTTP} = require('express-graphql');
 var schema = require('./graphql/bookSchemas');
 var cors = require("cors");
 
